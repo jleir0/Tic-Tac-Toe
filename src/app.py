adb7d91bda945f2f56d110c1002cc68bf7168bd1
jleir0 <40487776+jleir0@users.noreply.github.com>
2023-09-26 22:22:18 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from microservices.match.controller.match import match_api
 
 app = Flask(__name__)
 
-app.register_blueprint(match_api, url_prefix='/TicTacToe')
+app.register_blueprint(match_api, url_prefix='/')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000)
