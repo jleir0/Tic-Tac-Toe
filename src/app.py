1eb3ce5cd5bdfca9a930948eb867c0abc6e059d6
jleir0 <40487776+jleir0@users.noreply.github.com>
2023-09-27 11:20:43 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,4 +6,4 @@ tic_tac_toe = Flask(__name__)
 tic_tac_toe.register_blueprint(match_api, url_prefix='/')
 
 if __name__ == '__main__':
-    tic_tac_toe.run(host='0.0.0.0', port=5000, debug=True)
+    tic_tac_toe.run(host='127.0.0.1', port=5000)
