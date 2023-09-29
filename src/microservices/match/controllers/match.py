a99ea5ac07da2d98d2051e9a3ec38473a9ea84b4
jleir0 <leiro.jl@gmail.com>
2023-09-29 23:53:48 +0200

    feature/status(match.py): update endpoint

--- src/microservices/match/controllers/match.py
+++ src/microservices/match/controllers/match.py
@@ -15,8 +15,15 @@ class MatchResource(Resource):
 class MatchResource(Resource):
     def get(self):
         """This endpoint returns the current status of a given match."""
-        match_id = request.args.get('match_id') 
-        return 200, match_id
+        try:
+            s_matchId = request.args.get('matchId') 
+
+            response_data = db.session.get(s_matchId)
+            
+            return response_data, 200
+        except Exception as e:
+            db.session.rollback()
+            abort(500, f"An error occurred: {str(e)}")
 
 @match_api.route('/create')
 class MatchResource(Resource):
