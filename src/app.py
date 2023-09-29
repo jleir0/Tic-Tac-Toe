fde127ecd50c5128ed2bb19200be9141d2b8cc33
jleir0 <40487776+jleir0@users.noreply.github.com>
2023-09-29 17:03:37 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -9,5 +9,5 @@ api = Api(app, version='1.0', title='API de Juegos', description='API para gesti
 api.add_namespace(match_api)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
