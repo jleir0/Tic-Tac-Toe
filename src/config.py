c7585428cde4f3a8662feb60dc62b27e40a3c1e6
jleir0 <40487776+jleir0@users.noreply.github.com>
2023-09-29 20:14:56 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import os
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOSTNAME = os.getenv('DB_HOSTNAME')
-DB_PORT = int(os.getenv('DB_PORT'))
+DB_PORT = 5432
 
 SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/tic-tac-toe-db'
