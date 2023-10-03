83e71253e2deeedc1ec2ba2f3df25579c096703f
jleir0 <leiro.jl@gmail.com>
2023-10-03 21:40:53 +0200

    feature/move(match.py): add move endpoint and update the other two

--- src/models.py
+++ src/models.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import uuid
 
 db = SQLAlchemy()
 
 class Match(db.Model):
-    matchId = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
-    
+    matchId = db.Column(db.String(36), default=str(uuid.uuid4()), nullable=False)
+    playerId = db.Column(db.String(1), default="X", nullable=False)
+    Xmoves = db.Column(db.JSON, nullable=False, default=db.json.dumps([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+    Omoves= db.Column(db.JSON, nullable=False, default=db.json.dumps([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+    status = db.Column(db.String(10), default="playing", nullable=False)
