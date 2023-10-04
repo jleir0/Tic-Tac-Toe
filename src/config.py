8f247fa4f7d6404c6b9f767c755881cbfa3b4ce7
jleir0 <leiro.jl@gmail.com>
2023-10-04 21:15:21 +0200

    fix/move(match.py): add client certification

--- src/config.py
+++ src/config.py
@@ -12,7 +12,11 @@ DB_NAME = os.getenv('DB_USER')
 
 DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-engine = create_engine(DB_URI)
+PERM = os.getenv('PERM')
 
+engine = create_engine(
+    DB_URI,
+    connect_args={'ssl_ca': PERM}
+)
 Session = sessionmaker(bind=engine)
 session = Session()
