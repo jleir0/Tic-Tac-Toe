6d7d0a5441de3430636c8970131c0ee271becdbc
jleir0 <leiro.jl@gmail.com>
2023-10-04 20:10:50 +0200

    fix/move(match.py): fix db_uri

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
-import os
+import os 
+
+DB_HOST = os.getenv('DB_HOSTNAME')
+DB_PORT = os.getenv('DB_PORT',5432)
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOSTNAME = os.getenv('DB_HOSTNAME')
-DB_PORT = os.getenv('DB_PORT',5432)
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/tic-tac-toe-db'
+DB_NAME = os.getenv('DB_USER')
+
+DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
