6680065c870a48570a406d38ae25cc0a84939941
jleir0 <leiro.jl@gmail.com>
2023-10-04 21:02:28 +0200

    fix/move(match.py): UPDATE CONFIG

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from models import db
 from microservices.match.controllers.match import match_api
-from config import DB_URI, Session, engine
+from config import DB_URI
 
 app = Flask(__name__)
 
@@ -12,10 +12,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app, version='1.0', title='API de Juegos', description='API para gestionar juegos')
 api.add_namespace(match_api)
 
-if __name__ == '__main__':
-    
-    with app.app_context():
+db.init_app(app)
+
+with app.app_context():
         db.create_all()
-        
+
+if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000)
 
