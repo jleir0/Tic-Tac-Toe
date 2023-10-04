470fd6a0a3e1e173b1a3c76b6f5683eff45a3005
jleir0 <leiro.jl@gmail.com>
2023-10-04 20:54:54 +0200

    fix/move(match.py): UPDATE CONFIG

--- src/app.py
+++ src/app.py
@@ -2,21 +2,20 @@ from flask import Flask
 from flask_restx import Api
 from models import db
 from microservices.match.controllers.match import match_api
-from config import SQLALCHEMY_DATABASE_URI
+from config import DB_URI, Session, engine
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 api = Api(app, version='1.0', title='API de Juegos', description='API para gestionar juegos')
 api.add_namespace(match_api)
 
 if __name__ == '__main__':
+    
+    with app.app_context():
+        db.create_all()
+        
     app.run(host='0.0.0.0', port=5000)
 
--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DB_HOST = os.getenv('DB_HOSTNAME')
 DB_PORT = os.getenv('DB_PORT',5432)
@@ -8,5 +10,9 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 DB_NAME = os.getenv('DB_USER')
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
+engine = create_engine(DB_URI)
+
+Session = sessionmaker(bind=engine)
+session = Session()
