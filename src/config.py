7bc476e15068f3e3d19f6ef8a834ddd93448db2e
jleir0 <leiro.jl@gmail.com>
2023-10-04 23:03:01 +0200

    fix/move(match.py): punto anterior

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DB_HOST = os.getenv('DB_HOSTNAME')
-DB_PORT = os.getenv('DB_PORT',5432)
+DB_PORT = os.getenv('DB_PORT')
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -12,11 +12,7 @@ DB_NAME = os.getenv('DB_USER')
 
 DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-PERM = os.getenv('PERM')
+engine = create_engine(DB_URI)
 
-engine = create_engine(
-    DB_URI,
-    connect_args={'ssl_ca': PERM}
-)
 Session = sessionmaker(bind=engine)
 session = Session()
