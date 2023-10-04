2092956b7a6cf0515948050c72b61e7f0b492d1b
jleir0 <leiro.jl@gmail.com>
2023-10-04 20:14:16 +0200

    fix/move(match.py): fix db_uri

--- src/config.py
+++ src/config.py
@@ -8,5 +8,5 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 DB_NAME = os.getenv('DB_USER')
 
-DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
